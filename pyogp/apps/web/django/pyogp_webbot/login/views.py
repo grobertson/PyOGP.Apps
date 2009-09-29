fb686c206cbf656e4abfd76767374e1794a254a2
enus.linden <enus.linden@localhost>
2009-09-29 06:08:14 +0000

    super basic web login form running on pyogp. right now, the form just submits and waits while the console running the django app logs the pyogp instance

--- pyogp/apps/web/django/pyogp_webbot/login/views.py
+++ pyogp/apps/web/django/pyogp_webbot/login/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from pyogp.lib.client.agent import Agent
+from pyogp.lib.client.settings import Settings
+from pyogp.lib.base.helpers import Wait
+from pyogp.lib.base.exc import LoginError
 
-#from pyogp.lib.client.agent import Agent
-#from pyogp.lib.client.settings import Settings
+from eventlet import api
+
+import time
 
 def index(request):
     return login(request, kwargs)
@@ -23,13 +28,31 @@ def login_request(request, firstname = None, lastname = None, password = None, e
 
     try:
 
-        #settings = Settings()
-        raise Exception
+        bot_settings = Settings()
 
-    except Exception, e:
+        bot_settings.ENABLE_INVENTORY_MANAGEMENT = True
+        bot_settings.ENABLE_COMMUNICATIONS_TRACKING = False
+        bot_settings.ENABLE_OBJECT_TRACKING = False
+        bot_settings.ENABLE_UDP_LOGGING =True
+        bot_settings.ENABLE_EQ_LOGGING = True
+        bot_settings.ENABLE_CAPS_LOGGING = True
+        bot_settings.MULTIPLE_SIM_CONNECTIONS = False
 
-        error_message = 'login error message!'
+        client = Agent(settings = bot_settings, handle_signals = False)
+        #raise LoginError('this is BS')
 
-        return HttpResponse('would like to show the login form again, with any error message that\'s appropriate')
+        api.spawn(client.login, 'https://login.aditi.lindenlab.com/cgi-bin/login.cgi', firstname, lastname, password)
+
+        now = time.time()
+        start = now
+
+        while now - start < 15:
+            api.sleep()
+
+    except LoginError, e:
+
+        error_message = e
+
+        return HttpResponse('would like to show the login form again, with any error message that\'s appropriate, like: %s' % (error_message))
         #return HttpResponseRedirect(reverse('pyogp_webbot.login.views.login', kwargs={'firstname':firstname, 'lastname':lastname, 'error_message':error_message}))
     
