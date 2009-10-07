7cda438291069aec4a4bd59a0c29889873ccfa7f
kotler.linden <kotler.linden@localhost>
2009-10-07 01:00:06 +0000

    attempts at getting agent to say via chat interface

--- pyogp/apps/examples/chat_interface.py
+++ pyogp/apps/examples/chat_interface.py
@@ -5,9 +5,8 @@ import logging
 
 class Login(object):
     """ Initial login screen """
-    def __init__(self, parent, agent):
+    def __init__(self, parent):
         self.parent = parent
-        self.agent = agent
         self.frame = Frame(parent.frame)
         self.frame.pack()
  
@@ -21,66 +20,59 @@ class Login(object):
         self.last_name = Entry(self.frame)
         self.last_name.pack(side=TOP)
         
+        self.location_label = Label(self.frame, text="Login Location")
+        self.location_label.pack(side=TOP)
+        self.location = Entry(self.frame)
+        self.location.pack(side=TOP)
+                
         self.password_label = Label(self.frame, text="Password")
         self.password_label.pack(side=TOP)
         self.password = Entry(self.frame, show="*")
         self.password.bind("<Return>", self.login)
-        self.password.pack(side=TOP)
-
+        self.password.pack(side=TOP)        
+        
         self.login_button = Button(self.frame, text="Login", command=self.login)
         self.login_button.pack(side=TOP)
 
-    def login(self, entry=None):
+    def login(self, event=None):
         """ Logs in a pyogp agent """
-        self.agent.login(
-                         "https://login.agni.lindenlab.com/cgi-bin/login.cgi",
-                         self.first_name.get(), 
-                         self.last_name.get(),
-                         self.password.get(),
-                         start_location = None, 
-                         connect_region = True)
-        while self.agent.connected == False:
-            api.sleep(0)
-        while self.agent.region.connected == False:
-            api.sleep(0)      
-        self.agent.say("chat window")
+        start_location = self.location.get()
+        if start_location == "":
+            start_location = None
+        api.spawn(self.parent.login_agent, self.first_name.get(), self.last_name.get(), 
+                          self.password.get(), start_location)
         self.parent.init_chat()
         self.frame.pack_forget()
 
-class Logout(object):
-    """  Logout button """
-    def __init__(self, parent,agent):
-        self.parent = parent
-        self.agent = agent
-        self.frame = Frame(parent.frame)
-        self.frame.pack()
-        self.logout_button = Button(self.frame, text="Logout", command=self.logout)
-        self.logout_button.pack(side=BOTTOM)
-
-    def logout(self):
-        """ Logs out a PyOGP agent """
-        self.agent.logout()
-        self.parent.tear_down()
-
 class Chat(object):
     """ Chat widgets: window, entry, and agent list """    
-    def __init__(self, parent, agent):
+    def __init__(self, parent):
         self.parent = parent
-        self.agent = agent
         self.frame = Frame(parent.frame)
         self.frame.pack()
+        
         self.list = Listbox(self.frame)
         self.list.pack(side=RIGHT, fill=Y)
+        
         self.chat_log = Text(self.frame)
         self.chat_log.pack(side=TOP, fill=Y)        
+        
         self.chat_entry = Entry(self.frame)
         self.chat_entry.bind("<Return>", self.send_chat)
-        self.chat_entry.pack(side=BOTTOM, fill=X)
-
+        self.chat_entry.pack(side=TOP, fill=X)
+        
+        self.logout_button = Button(self.frame, text="Logout",
+                                    command=self.logout)
+        self.logout_button.pack(side=BOTTOM)
+        
     def send_chat(self, event):
         """ sends a chat message """
-        self.agent.say(self.chat_entry.get())
+        self.parent.agent.say(self.chat_entry.get())
         event.widget.delete(0, END)
+    
+    def logout(self):
+        self.parent.agent.logout()
+        self.parent.tear_down()
 
 class Chat_window(object):
     """ Chat window """
@@ -88,22 +80,40 @@ class Chat_window(object):
         self.frame = Frame(frame)
         self.frame.pack()
         self.agent = Agent()
-        self.login = Login(self, self.agent)
+        self.login = Login(self)
         self.logout = None
         self.chat = None 
 
-    def init_chat(self):
+    def init_chat(self,):
         """ Initializes a chat widgets after login """
-        self.chat = Chat(self, self.agent)
-        self.logout = Logout(self, self.agent)
+        self.chat = Chat(self)
         
+    def login_agent(self, firstname, lastname, password, start_location):
+        self.agent.login( 
+                    "https://login.agni.lindenlab.com/cgi-bin/login.cgi",
+                    firstname=firstname, 
+                    lastname=lastname,
+                    password=password,
+                    start_location=start_location, 
+                    connect_region=True)
+        while self.agent.connected == False:
+            api.sleep(0)
+        while self.agent.region.connected == False:
+            api.sleep(0)
+        while connected:
+            api.sleep(0)
 
     def tear_down(self):
         """ Tears down chat widgets and logout widget after logout """
         self.chat.frame.pack_forget()
-        self.logout.frame.pack_forget()
-
+        
 def main():
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG) # seems to be a no op, set it for the logger
+    formatter = logging.Formatter('%(asctime)-30s%(name)-30s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    logging.getLogger('').setLevel(logging.DEBUG)
     root = Tk()
     Chat_window(root)
     root.title("PyOGP Chat interface")
